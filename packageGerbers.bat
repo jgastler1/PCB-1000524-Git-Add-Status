d2e9fb5f61da7fa0d1d695f9d246865c83c90d6c
jgastler <jason.gastler@synapsewireless.com>
2020-11-20 15:58:09 -0600

    Initial Commit from Project Creator

---
+++ packageGerbers.bat
@@ -0,0 +1,32 @@
+cd 1000524_Documents\1000524-GERBERS
+del *.zip
+del *.rul
+del *.APR_LIB
+del *.rep
+del *.ldp
+del *.apr
+del *.drr
+del "Status Report.txt"
+cd ..
+cd 1000524-ODB++_Files
+del *.zip
+del *.rul
+del *.rep
+del *.tgz
+del "Status Report.txt"
+cd ..
+cd 1000524-STENCIL
+del *.zip
+del *.rul
+del *.APR_LIB
+del *.rep
+del *.ldp
+del *.apr
+del *.drr
+del *.extrep
+del "Status Report.txt"
+cd ..
+del "Status Report.txt"
+cd..
+cd 1000525_Documents
+del "Status Report.txt"
